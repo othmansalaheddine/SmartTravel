bf009825bf542c48f02c2783e4dd6ae3791be2a5
othman salaheddine <othman.salaheddine@gmail.com>
2023-12-22 00:02:46 +0100

    othman

--- SmartTravel.sql
+++ SmartTravel.sql
@@ -1,157 +1,159 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 21, 2023 at 03:28 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+@@ -0,0 +1,158 @@
+CREATE DATABASE smarttravel;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+--@block
+CREATE TABLE ville(
+    nom_ville VARCHAR(255),
+    PRIMARY KEY(nom_ville)
+
+)
+
+--@block
+INSERT INTO ville (nom_ville) VALUES
+('Casablanca'),
+('Fez'),
+('Tangier'),
+('Marrakesh'),
+('Salé'),
+('Meknes'),
+('Rabat'),
+('Oujda'),
+('Kenitra'),
+('Agadir'),
+('Tetouan'),
+('Temara'),
+('Safi'),
+('Mohammedia'),
+('Khouribga'),
+('El Jadida'),
+('Beni Mellal'),
+('Aït Melloul'),
+('Nador'),
+('Dar Bouazza'),
+('Settat'),
+('Berrechid'),
+('Khemisset'),
+('Inezgane'),
+('Ksar El Kebir'),
+('Larache'),
+('Guelmim'),
+('Khenifra'),
+('Berkane'),
+('Taourirt'),
+('Bouskoura'),
+('Fquih Ben Salah'),
+('Dcheira El Jihadia'),
+('Oued Zem'),
+('El Kelaa Des Sraghna'),
+('Sidi Slimane'),
+('Errachidia'),
+('Guercif'),
+('Oulad Teima'),
+('Ben Guerir'),
+('Tifelt'),
+('Lqliaa'),
+('Taroudant'),
+('Sefrou'),
+('Essaouira'),
+('Fnideq'),
+('Sidi Kacem'),
+('Tiznit'),
+('Tan-Tan'),
+('Ouarzazate'),
+('Souk El Arbaa'),
+('Youssoufia'),
+('Lahraouyine'),
+('Martil'),
+('Ain Harrouda'),
+('Suq as-Sabt Awlad an-Nama'),
+('Skhirat'),
+('Ouazzane'),
+('Benslimane'),
+('Al Hoceima'),
+('Beni Ansar'),
+('Mdieq'),
+('Sidi Bennour'),
+('Midelt'),
+('Azrou'),
+('Drargua');
+
+--@block
+
+CREATE TABLE entreprise (
+    short_name VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    img VARCHAR(255) NOT NULL,
+    PRIMARY KEY (short_name, full_name)
+);
+
+--@block
+INSERT INTO entreprise (id, short_name , full_name, img) VALUES 
+(1, 'CTM','Compagnie de Transports au Maroc', LOAD_FILE('imgs/ctm.jpg')),
+(2, 'TJV' ,'TajVoyage', LOAD_FILE('imgs/taj.jpg')),
+(3,'BAS', 'Bismi Allah Salama', LOAD_FILE('imgs/bismilah.jpg')),
+(4,'SF', 'SAT First', LOAD_FILE('imgs/SAT_First.jpg')),
+(5,'Ghazala', 'Ghazala', LOAD_FILE('imgs/ghazala.jpg')),
+(6, 'AWA','Al Wissam Addahabi', LOAD_FILE('imgs/AlWissamAddahabi.jpg'));
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+--@block
+CREATE TABLE `bus`(
+    num_bus INT NOT NULL AUTO_INCREMENT,
+    matricule INT NOT NULL, 
+    nom_entreprise VARCHAR(255) NOT NULL,
+    capacite INT NOT NULL,
+    PRIMARY KEY (num_bus),
+    FOREIGN KEY (nom_entreprise) REFERENCES entreprise(short_name)
+);
+--@block
+CREATE TABLE route(
+    id INT NOT NULL AUTO_INCREMENT,
+    v_depart VARCHAR(30),
+    v_arrive VARCHAR(30),
+    distance INT(5),
+    duration time,
+    UNIQUE (id),
+    PRIMARY KEY(v_depart , v_arrive),
+    FOREIGN KEY(v_depart) REFERENCES ville(nom_ville),
+    FOREIGN KEY(v_arrive) REFERENCES ville(nom_ville)
+);
 
---
--- Database: `brief9`
---
+--@block
+INSERT INTO route (v_depart, v_arrive, distance, duration) VALUES
+('Casablanca', 'Fez', 320, '04:30:00'),
+('Fez', 'Marrakesh', 530, '07:15:00'),
+('Tangier', 'Agadir', 680, '09:00:00'),
+('Marrakesh', 'Rabat', 330, '04:45:00'),
+('Oujda', 'Kenitra', 610, '08:30:00'),
 
--- --------------------------------------------------------
+('Rabat', 'Tetouan', 290, '04:00:00'),
+('Agadir', 'Oujda', 850, '11:30:00'),
+('Kenitra', 'Casablanca', 200, '03:00:00'),
+('Tetouan', 'Essaouira', 540, '07:45:00'),
+('Temara', 'Safi', 150, '02:15:00'),
 
---
--- Table structure for table `bus`
---
+('Safi', 'Meknes', 380, '05:30:00'),
+('Mohammedia', 'Khouribga', 220, '03:15:00'),
+('Khouribga', 'El Jadida', 270, '03:45:00'),
+('El Jadida', 'Beni Mellal', 320, '04:30:00'),
+('Beni Mellal', 'Aït Melloul', 590, '08:15:00'),
 
-CREATE TABLE `bus` (
-  `ID` int(11) NOT NULL,
-  `bus_number` varchar(255) DEFAULT NULL,
-  `license_plate` varchar(255) DEFAULT NULL,
-  `company` varchar(255) DEFAULT NULL,
-  `capacity` int(11) DEFAULT NULL,
-  `fk_company` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+('Nador', 'Dar Bouazza', 760, '10:45:00'),
+('Dar Bouazza', 'Settat', 210, '03:00:00'),
+('Settat', 'Berrechid', 60, '01:00:00'),
+('Berrechid', 'Khemisset', 150, '02:15:00'),
+('Inezgane', 'Ksar El Kebir', 400, '05:45:00');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `city`
---
-
-CREATE TABLE `city` (
-  `name_city` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `company`
---
-
-CREATE TABLE `company` (
-  `company_id` int(11) NOT NULL,
-  `company_name` varchar(255) DEFAULT NULL,
-  `company_image` blob DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `horaire`
---
-
-CREATE TABLE `horaire` (
-  `schedule_id` int(11) NOT NULL,
-  `bus_id` int(11) DEFAULT NULL,
-  `route_id` int(11) DEFAULT NULL,
-  `departure_date` date DEFAULT NULL,
-  `departure_time` time DEFAULT NULL,
-  `arrival_time` time DEFAULT NULL,
-  `seats_available` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `route`
---
-
-CREATE TABLE `route` (
-  `route_id` int(11) NOT NULL,
-  `dist` float DEFAULT NULL,
-  `departure_city` varchar(255) DEFAULT NULL,
-  `destination_city` varchar(255) DEFAULT NULL,
-  `duree` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `bus`
---
-ALTER TABLE `bus`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_company` (`fk_company`);
-
---
--- Indexes for table `city`
---
-ALTER TABLE `city`
-  ADD PRIMARY KEY (`name_city`);
-
---
--- Indexes for table `company`
---
-ALTER TABLE `company`
-  ADD PRIMARY KEY (`company_id`);
-
---
--- Indexes for table `horaire`
---
-ALTER TABLE `horaire`
-  ADD PRIMARY KEY (`schedule_id`),
-  ADD KEY `bus_id` (`bus_id`),
-  ADD KEY `route_id` (`route_id`);
-
---
--- Indexes for table `route`
---
-ALTER TABLE `route`
-  ADD PRIMARY KEY (`route_id`),
-  ADD KEY `departure_city` (`departure_city`),
-  ADD KEY `destination_city` (`destination_city`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bus`
---
-ALTER TABLE `bus`
-  ADD CONSTRAINT `fk_company` FOREIGN KEY (`fk_company`) REFERENCES `company` (`company_id`);
-
---
--- Constraints for table `horaire`
---
-ALTER TABLE `horaire`
-  ADD CONSTRAINT `horaire_ibfk_1` FOREIGN KEY (`bus_id`) REFERENCES `bus` (`ID`),
-  ADD CONSTRAINT `horaire_ibfk_2` FOREIGN KEY (`route_id`) REFERENCES `route` (`route_id`);
-
---
--- Constraints for table `route`
---
-ALTER TABLE `route`
-  ADD CONSTRAINT `route_ibfk_1` FOREIGN KEY (`departure_city`) REFERENCES `city` (`name_city`),
-  ADD CONSTRAINT `route_ibfk_2` FOREIGN KEY (`destination_city`) REFERENCES `city` (`name_city`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--@block
+CREATE TABLE horaire(
+    id INT NOT NULL AUTO_INCREMENT,
+    s_time TIMESTAMP,
+    arrive_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    d_date DATE,
+    prix INT(3),
+    numero_bus INT NOT NULL,
+    route_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(numero_bus) REFERENCES bus(num_bus),
+    FOREIGN KEY(route_id) REFERENCES route(id)
+);
